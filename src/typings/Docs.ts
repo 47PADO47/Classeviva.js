b132a4031c23163563a57badeb720d89792d19d2
PADO <62028267+47PADO47@users.noreply.github.com>
2022-07-11 23:34:49 +0200

    🏷️ Create Docs.ts types

---
+++ src/typings/Docs.ts
@@ -0,0 +1,14 @@
+import { Rest, Web } from "../../index";
+
+type Class = Rest | Web;
+
+interface FScheckOptions {
+    path: string;
+    type?: FScheckType;
+};
+type FScheckType = 'dir' | 'file';
+
+export {
+    Class,
+    FScheckOptions,
+}
