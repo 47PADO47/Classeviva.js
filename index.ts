5155b9f3b69888ba9171c43e86415fd2daa3f601
PADO <62028267+47PADO47@users.noreply.github.com>
2023-05-14 20:43:00 +0200

    ✨ Export Tibidabo class

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 export { default as Rest } from './src/classes/Rest';
 export { default as Web } from './src/classes/Web';
+export { default as Tibidabo } from './src/classes/Tibidabo';
 
 export { default as Enums } from './src/Enums';
