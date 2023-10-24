0636503e254eeb239153ce787a3aeb1906fa2386
PADO <62028267+47PADO47@users.noreply.github.com>
2023-10-24 14:41:12 +0200

    💥 Create HTTPClient

---
+++ src/base/http.ts
@@ -0,0 +1,29 @@
+import { Client, Dispatcher } from "undici";
+import { HTTPClientOptions, IHTTPCLient } from "../types/http";
+
+class HTTPClient extends Client implements IHTTPCLient {
+    url: string;
+    debug: boolean;
+    constructor({ url, ...options }: HTTPClientOptions) {
+        super(url, options.options)
+        
+        this.url = url;
+        this.debug = options.debug;
+    }
+
+    async request(options: Dispatcher.RequestOptions): Promise<Dispatcher.ResponseData> {
+        this.log(options.method, options.path, JSON.stringify(options.body || {}));
+
+        const response = await super.request(options);
+        this.log(response.statusCode, response.headers['content-type']);
+
+        return response;
+    }
+
+    log(...args: any[]): void {
+        if (!this.debug) return;
+        console.log('[\x1b[36mHTTP Client\x1b[0m]', ...args);
+    }
+}
+
+export default HTTPClient;
