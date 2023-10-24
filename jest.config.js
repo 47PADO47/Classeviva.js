235ae579d9ac8ecd2e9fbac8734e8e4d40f02953
PADO <62028267+47PADO47@users.noreply.github.com>
2023-10-24 15:33:04 +0200

    🔧 Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  transform: {'^.+\\.ts?$': 'ts-jest'},
+  testEnvironment: 'node',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'js']
+};
