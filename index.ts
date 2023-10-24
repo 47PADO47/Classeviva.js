7199101e1946150d5dcb8badb8de9a4926558c37
PADO <62028267+47PADO47@users.noreply.github.com>
2023-10-24 15:34:20 +0200

    🚚 Move index.ts into src/index.ts

--- index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Rest } from './src/classes/Rest';
-export { default as Web } from './src/classes/Web';
-export { default as Tibidabo } from './src/classes/Tibidabo';
-
-export * as Enums from './src/base/enums';
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as Rest } from './classes/Rest';
+export { default as Web } from './classes/Web';
+export { default as Tibidabo } from './classes/Tibidabo';
+
+export * as Enums from './base/enums';
